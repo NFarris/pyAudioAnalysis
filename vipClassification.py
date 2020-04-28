6b7b7ffbdf335146429380a2eaab471d09f3b93b
nfarris6 <nfarris6@gatech.edu>
2020-04-28 19:16:11 -0400

    init

---
+++ vipClassification.py
@@ -0,0 +1,24 @@
+from pyAudioAnalysis import audioTrainTest as aT
+from pathlib import Path
+
+directory_in_str = "emotionData"
+pathlist = Path(directory_in_str).glob('**/*.wav')
+print(pathlist)
+
+listOfDirs = []
+
+for path in pathlist:
+    path_in_str = str(path)
+    parent = path.parents[0]
+    if str(parent) not in listOfDirs:
+        listOfDirs.append(str(parent))
+
+mtWin = 1.0
+mtStep = 1.0
+stWin = aT.shortTermWindow
+stStep = aT.shortTermStep
+classifierType = "svm"
+modelName = "svmAAFL_1.0"
+beat = True
+
+aT.extract_features_and_train(listOfDirs, mtWin, mtStep, stWin, stStep, classifierType, modelName, beat)
